6cbccc55f2ea004178fa6d5f9fb950eee29f172b
Nikhil Dwivedi <niks.dwivedi@gmail.com>
2013-02-27 18:53:31 +1100

    Adding Property Class
    Adding property classes to generate mapping class. I'll call this manual
    mapping ... wuhuuuuu

---
+++ ObjectMappingClassGenerator/ObjectMappingClassGenerator/PropertyClasses/PropertyClass.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ObjectMappingClassGenerator.PropertyClasses
+{
+    public class PropertyClass
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public Cc1 Prop9 { get; set; }
+        public Cc1 Prop10 { get; set; }
+        public Cc2 Prop11 { get; set; }
+        public Cc2 Prop12 { get; set; }
+        public string Prop13 { get; set; }
+        public Cc3 Prop14 { get; set; }
+        public Cc4 Prop15 { get; set; }
+        public Cc5 Prop16 { get; set; }
+        public DateTimeOffset Prop17 { get; set; }
+    }
+    public class Cc1
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public int Prop9 { get; set; }
+        public string Prop10 { get; set; }
+        public int Prop11 { get; set; }
+        public double Prop12 { get; set; }
+    }
+    public class Cc2
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public int Prop9 { get; set; }
+        public string Prop10 { get; set; }
+        public string Prop11 { get; set; }
+        public int Prop12 { get; set; }
+        public string Prop13 { get; set; }
+        public int Prop14 { get; set; }
+        public double Prop15 { get; set; }
+        public Cc4 Prop16 { get; set; }
+    }
+
+    public class Cc3
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+    }
+
+    public class Cc4
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public Cc5 Prop9 { get; set; }
+    }
+
+    public class Cc5
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public string Prop9 { get; set; }
+    }
+}
---
+++ ObjectMappingClassGenerator/ObjectMappingClassGenerator/PropertyClasses/PropertyClassGenerated.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ObjectMappingClassGenerator.PropertyClasses
+{
+    public class PropertyClassGenerated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public Cc1Generated Prop9 { get; set; }
+        public Cc1Generated Prop10 { get; set; }
+        public Cc2Generated Prop11 { get; set; }
+        public Cc2Generated Prop12 { get; set; }
+        public string Prop13 { get; set; }
+        public Cc3Generated Prop14 { get; set; }
+        public Cc4Generated Prop15 { get; set; }
+        public Cc5 Prop16 { get; set; }
+        public DateTimeOffset Prop17 { get; set; }
+    }
+    public class Cc1Generated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public int Prop9 { get; set; }
+        public string Prop10 { get; set; }
+        public int Prop11 { get; set; }
+        public double Prop12 { get; set; }
+    }
+    public class Cc2Generated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public int Prop9 { get; set; }
+        public string Prop10 { get; set; }
+        public string Prop11 { get; set; }
+        public int Prop12 { get; set; }
+        public string Prop13 { get; set; }
+        public int Prop14 { get; set; }
+        public double Prop15 { get; set; }
+        public Cc4Generated Prop16 { get; set; }
+    }
+
+    public class Cc3Generated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+    }
+
+    public class Cc4Generated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public Cc5 Prop9 { get; set; }
+    }
+
+    public class Cc5Generated
+    {
+        public int Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+        public double Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public int Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public string Prop9 { get; set; }
+    }
+}
